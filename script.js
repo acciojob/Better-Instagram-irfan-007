edd035a300859ba04284f0858acd378972c86969
irfan <85334710+irfan-007@users.noreply.github.com>
2024-07-29 19:33:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-//your code here
 let drags = ["dragover", "dragenter", "drop"];
 let images = document.querySelectorAll(".image");
-let parent = document.getElementById("parent");
 
 images.forEach((img) => {
   img.addEventListener("dragstart", (e) => {
-    let t = 0;
-    let prt = document.querySelectorAll(".image");
-    for (let i = 0; i < prt.length; i++) {
-      if (prt[i] == e.target) t = i;
-    }
     e.dataTransfer.setData("id", e.target.id);
-    e.dataTransfer.setData("index", t);
   });
 });
 
@@ -21,14 +13,12 @@ for (let img of images) {
       e.preventDefault();
       if (drg == "drop") {
         let id = e.dataTransfer.getData("id");
-        let index = e.dataTransfer.getData("index");
         let dragElem = document.getElementById(id);
-        let dropElem = e.target;
-        // console.log(index);
-        parent.appendChild(dragElem.cloneNode(true));
-        parent.replaceChild(dragElem, dropElem);
-        parent.insertBefore(dropElem, parent.childNodes[index]);
-        parent.removeChild(parent.lastElementChild);
+        let dropElem = document.getElementById(e.target.id);
+        let dragContainer = dragElem.parentNode;
+        let dropContainer = dropElem.parentNode;
+        dragContainer.appendChild(dropElem);
+        dropContainer.appendChild(dragElem);
       }
     });
   }
